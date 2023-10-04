52d1c7df348902ea97e7f943c0ca72f7b15f813a
ReemasXY <sameermhr345@gmail.com>
2023-10-04 12:31:59 +0545

    changes in message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Message() {
   return (
     <div>
-      hello this is sameer 
+      hello this is sameer i am ready to make changes in git now
     </div>
   )
 }
