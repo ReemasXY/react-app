31a1468c8212c9ed68484df4c8dfa81c890ff091
ReemasXY <sameermhr345@gmail.com>
2023-10-09 19:43:30 +0545

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Message from "./components/Message";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Message />
-      <ListGroup />
     </>
   );
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export default function Message() {
+
+ 
+  const a = () => {
+    console.log("a");
+  };
+
+  const b = () => {
+    console.log("b");
+  };
   return (
-    <div>
-      hello this is sameer i am ready to make changes in git now
-    </div>
-  )
+    <>
+      <nav>
+        <Link to="/">home</Link>
+        <Link to="/about">about</Link>
+        <Link to="/search">search</Link>
+      </nav>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/about"
+            element={<h1>Hello I am about section</h1>}
+          ></Route>
+          <Route
+            exact
+            path="/"
+            element={<h1>Hello I am homet section</h1>}
+          ></Route>
+          <Route
+            exact
+            path="/search"
+            element={<h1>Hello I am search section</h1>}
+          ></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
